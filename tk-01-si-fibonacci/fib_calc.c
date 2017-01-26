92b48bcebbaed3c742fe58d7e651c20bb9e0a1d9
tamasborbas <tamas.borbas@incquerylabs.com>
2017-01-26 17:40:07 +0100

    Initial version of fib_calc project

---
+++ tk-01-si-fibonacci/fib_calc.c
@@ -0,0 +1,3 @@
+#include "fib_calc.h"
+
+/* The fib_calc method shall be defined here */
---
+++ tk-01-si-fibonacci/main.c
@@ -0,0 +1,10 @@
+/* DO NOT MODIFY THIS FILE! */
+#include <stdio.h>
+#include "fib_calc.h"
+
+int main() {
+	unsigned int array[15] = {0};
+	printf("%u", fib_calc(array, 15)); /* Expected output: 377 */
+
+	return 0;
+}
